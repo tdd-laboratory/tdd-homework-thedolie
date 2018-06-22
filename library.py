c2461d6022a55140d979f723c98b8aef640a2ba2
Joe <josephmgenereux@gmail.com>
2018-06-22 08:56:31 -0600

    finished homework

--- library.py
+++ library.py
@@ -5,6 +5,8 @@ _mixed_ordinal_pat = _whole_word(r'-?\d+(st|th|nd|rd)')
 _integer_pat = _whole_word(r'\d+')
 _floating_point_after_pat = re.compile(r'\.\d+[^a-zA-Z.]')
 _floating_point_before_pat = re.compile(r'(?<=\d\.)')
+_date_iso8601_pat = _whole_word(r'\d{4}-(0\d|1[0-2])-(0[1-9]|[12][0-9]|3[01])')
+_dates_valid = _whole_word(r'\d{2} (J[au]n|Feb|Ma[ry]|Apr|Jul|Aug|Sep|Oct|Nov|Dec) \d{4}')
 
 def mixed_ordinals(text):
     '''Find tokens that begin with a number, and then have an ending like 1st or 2nd.'''
@@ -29,3 +31,13 @@ def scan(text, *extractors):
     for extractor in extractors:
         for item in extractor(text):
             yield item
+
+def dates_iso8601(text):
+    '''Find the date'''
+    for match in _date_iso8601_pat.finditer(text):
+        yield('date', match)
+
+def dates_valid_func(text):
+    '''Find correct date in 25 Jan 2017'''
+    for match in _dates_valid.finditer(text):
+        yield('date', match)
--- library_test.py
+++ library_test.py
@@ -7,7 +7,7 @@ began in 1845 (the twenty-second anniversary of the Mexican Revolution), and
 is the 1st example of a national independence holiday becoming popular in the
 Western Hemisphere. (The Fourth of July didn't see regular celebration in the
 US until 15-20 years later.) It is celebrated by 77.9% of the population--
-trending toward 80.                                                                
+trending toward 80.
 '''
 
 class TestCase(unittest.TestCase):
@@ -29,6 +29,38 @@ class TestCase(unittest.TestCase):
     # Third unit test; prove that if we look for integers where there are none, we get no results.
     def test_no_integers(self):
         self.assert_extract("no integers", library.integers)
+
+    # Forth unit test; if we have a date in a text, extract the date from the text.
+    def test_extract_date(self):
+      self.assert_extract('I was born on 2015-07-25.', library.dates_iso8601, '2015-07-25')
+
+    # Fifth test; if the date is incorrect
+    def test_wrong_date(self):
+        self.assert_extract("2013-12-25.", library.dates_iso8601)
+
+    def test_dates_valid(self):
+        self.assert_extract('I was born on 25 Jan 2017.', library.dates_valid_func, '25 Jan 2017')
+
+    def test_dates_with_timestamps(self):
+        self.assert_extract('I was born on 25 Jan 2017.', library.dates_valid_with_timestamp)
+
+    def test_dates_with_timesstamps_end_with_seconds(self):
+        self.assert_extract('I was born on 25 Jan 2017.', library.dates_valid_with_timestamp_with_time, '25 Jan 2017')
+
+    def test_dates_spaceing(self):
+        self.assert_extract('I was born on 25 Jan 2017.', library.dates_valid_spacing, '25 Jan 2017')
+
+    def test_matching_month(self):
+        self.assert_extract('I was born on 25 Jan 2017.', library.dates_match_month, '25 Jan 2017')
+
+    def test_dates_comma(self):
+        self.assert_extract('I was born on 25 Jan 2017.', library.dates_valid_comma_seperation, '25 Jan 2017')
+
+    def test_handle_colons(self):
+      self.assert_extract('18:22:19.123', library.dates_valid_colons)
+
+    def test_handle_time_zone(self):
+      self.assert_extract('18:22:19.123', library.dates_valid_colons)
 
 
 if __name__ == '__main__':
